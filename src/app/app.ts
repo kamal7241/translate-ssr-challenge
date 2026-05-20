1bcbf109b5e43425c80381af6c8cfccd180dacb4
kkhalil <kkhalil@moj.gov.sa>
2026-05-20 12:51:06 +0300

    feat:init ng-translate

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
+
+
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
